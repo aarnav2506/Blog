344bff9d62469c580ba17db20cb1f9b2b9b38d49
Aarnav <137188579+aarnav2506@users.noreply.github.com>
2026-06-19 14:03:47 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener("install", (event) => {
+  console.log("Service Worker installed for Aarnav Portfolio");
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
